5e872929385b0ca72c7373d96ea762c20934a728
NetworkPistolero <@NetworkPistol>
2019-07-14 23:30:42 -0700

    Forgot to save read_json_test.py.  Include reminder comments

--- read_json_test.py
+++ read_json_test.py
@@ -6,9 +6,12 @@ import json
 with open("./api_json/dnac_intent_v1_3.json") as file_handle:
     rest_resource_dict = json.load(file_handle)
 
+# Checking if its a dict
 print("REST RESOURCE")
 print(type(rest_resource_dict))
 
+# Printing Depth 1 Keys.
+# To be continued
 for i in rest_resource_dict:
     print(i)
     print("-" * 10)
